e9a8c8bc553fd48729d05407676ed3abb58483e0
newbalancem5 <newbalancem5@gmail.com>
2019-03-10 17:50:11 +0300

    upd

--- lib/AddNewTask.dart
+++ lib/AddNewTask.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class AddNewTask extends StatelessWidget {
+class AddNewTask extends StatefulWidget {
+@override
+_AddNewTaskState createState() => _AddNewTaskState();   
+}
+
+class _AddNewTaskState extends State<AddNewTask> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
