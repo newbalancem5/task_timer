188a275f710537832035d49c872e808f00dd903c
newbalancem5 <newbalancem5@gmail.com>
2019-04-30 17:48:01 +0300

    add new elem

---
+++ lib/Task/InfoContainer.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+
+ class TaskInfoBody extends StatelessWidget {
+     
+final String title;
+final String body;
+// final String to;
+
+TaskInfoBody(this.title, this.body);
+   
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 420.0,
+      child: Stack(
+        children: <Widget>[
+          ClipPath(
+            child: Container(
+              height: 370.0,
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0.0, 10.0),
+                    blurRadius: 10.0)
+              ]),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                          const Color(0xFFf45d27),
+                          const Color(0xFFf5851f)
+                        ],
+                            stops: [
+                          0.0,
+                          0.9
+                        ],
+                            begin: FractionalOffset(0.0, 0.0),
+                            end: FractionalOffset(0.0, 1.0))),
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 120.0, left: 95.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            this.title,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15.0,
+                                fontFamily: "SF-Pro-Display-Bold"),
+                          ),
+                          Text(
+                            this.body,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 45.0,
+                                fontFamily: "SF-Pro-Display-Bold"),
+                          ),
+                           Text(
+                            'text',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 45.0,
+                                fontFamily: "SF-Pro-Display-Bold"),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 370.0,
+            right: 100,
+            child: FractionalTranslation(
+              translation: Offset(0.0, -0.5),
+              child: Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: 16.0,
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: RaisedButton(
+                      onPressed: () {},
+                      color: Color(0xFFE52020),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 80.0),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "Начать",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontFamily: "SF-Pro-Display-Bold"),
+                          ),
+                          ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
