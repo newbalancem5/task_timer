02acb93836d7983a5d194fec8a38944e160df245
newbalancem5 <newbalancem5@gmail.com>
2019-04-30 17:48:12 +0300

    add buttom element

---
+++ lib/Task/infoContainerBottom.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+
+class TaskInfoBottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data',
+      style: TextStyle(
+        color: Colors.white
+      ),),
+      
+    );
+  }
+} 
