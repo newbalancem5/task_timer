d53429aa04b0a168afef1b6c4e7d128155779daf
newbalancem5 <newbalancem5@gmail.com>
2019-05-05 23:58:58 +0300

    Create new element

---
+++ lib/Task/AddTask.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:newba/Task/AddNewTask.dart';
+import 'package:newba/Message/Message.dart';
+
+class AddButtonTask extends StatelessWidget {
+ final   List<Message> message;
+   AddButtonTask(this.message);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  FloatingActionButton(  
+         child: Icon(Icons.add),
+        onPressed: () async {
+          Message messages = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) => AddNewTask(),              
+            ),
+          );
+          
+          if (message != null) {
+          message.add(messages);
+          
+            Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Your message has been sent"),
+              backgroundColor: Colors.green,
+              ),
+            );
+          }
+        },     
+        tooltip: 'Increment',
+        backgroundColor: Colors.red[300],      
+    );
+
+  }
+}
