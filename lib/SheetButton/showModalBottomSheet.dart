87e828b4d856653061d60d3f61fed3e8ad854984
newbalancem5 <newbalancem5@gmail.com>
2019-05-05 23:59:27 +0300

    Update showModalBottomSheet.dart

--- lib/SheetButton/showModalBottomSheet.dart
+++ lib/SheetButton/showModalBottomSheet.dart
@@ -1,67 +1,44 @@
 import 'package:flutter/material.dart';
 
 
-class ButtonTimer extends StatelessWidget {
+
+class BattonAddNewTime extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Material(
-                  borderRadius: BorderRadius.circular(40.0),
+     child:  Material(
+                  borderRadius: BorderRadius.circular(100.0),
                   shadowColor: Colors.pinkAccent,
-                  elevation: 9.0,
-                  color: Colors.pinkAccent,
+                  elevation: 7.0,
+                  color: Colors.pink[50],
                   child: MaterialButton(
-                    minWidth:240.0,
+                    minWidth:160.0,
                     height: 50.0,
                     onPressed: (){
-                      _bottomSheedPressed();
-                    },
-                    child: Text('Добавить таймер',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 17),
-                    ),
-                  ),
-                ),      
-    );
-  }
-    _bottomSheedPressed(){
-      
-   showModalBottomSheet(
+                      showModalBottomSheet(
     context: context,
     builder: (BuildContext context) {      
     return Container(
             decoration: BoxDecoration(
-              color: Colors.greenAccent,              
+              color: Colors.white,              
               borderRadius: BorderRadius.vertical(
-                top: Radius.circular(40)                
+                top: Radius.circular(45)                
               )
             ),
-            height: 300, 
-            child: Column(
+            height: 300,             
+            child: Column(                            
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  
                   Container(
-                    child: Container(
-                      child: IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: (){
-                          
-                        },
-                      ),                      
-                    ),
-                   ),
-                   Container(
-                     child: Text('Количество  минут'),
-
-                   ),
-                   Container(child: IconButton(
-                   icon: Icon(Icons.minimize),
-                   onPressed: (){
-                     
-                   },)),
+                     padding: EdgeInsets.only(left: 16, right: 16, bottom: 180),
+                     child: Title(
+                       child: Text('Выберите время для задачи',
+                    style: TextStyle(
+                      fontSize: 25,
+                    ),),
+                    color: Colors.black,
+                     ),
+                  ),
                   Material(
                     borderRadius: BorderRadius.circular(40.0),
                     shadowColor: Colors.red,
@@ -80,5 +57,16 @@ class ButtonTimer extends StatelessWidget {
             ),
           );
    });
+                    },
+                    child: Text('Добавить таймер',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17),
+                    ),
+                  ),
+                ),
+      
+    );
 }
 }
