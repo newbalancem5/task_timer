ca5dbd80c1c4a102e260234b175057b21e017f1c
newbalancem5 <newbalancem5@gmail.com>
2019-05-15 19:01:46 +0300

    upd

--- lib/TaskInfo/infoContainerBottom.dart
+++ lib/TaskInfo/infoContainerBottom.dart
@@ -8,30 +8,23 @@ class TaskInfoBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    return new Container(
-      height: 300.0,
-    padding: EdgeInsets.all(50),
-      child: Stack(
-        children: <Widget>[
-          Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              color: Colors.white,
-              
-              ),
-              child:
-               Text('data',
-              style: TextStyle(
-              color: Colors.black,
-              fontSize: 30)
-              
-              ),
-          //  color: Colors.white,
-                     
-          ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 10),
+      child: Material(
+        elevation: 19.0,
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+        color: Colors.white,
+        shadowColor: Colors.black,
+        child: Container(
+          width: 200,
+          height: 200,
+          color: Colors.white,
+          child: 
+          Text('data',
+          style: TextStyle(
+            fontSize: 30
+          ),),
           
-        ],
+        ),
       ),
     );
   }
