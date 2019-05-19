f09f63ef13d4b39e6ed6ba455614d8b0332451aa
newbalancem5 <newbalancem5@gmail.com>
2019-05-19 21:35:33 +0300

    Update infoContainerBottom.dart

--- lib/TaskInfo/infoContainerBottom.dart
+++ lib/TaskInfo/infoContainerBottom.dart
@@ -3,27 +3,33 @@ import 'package:flutter/material.dart';
 
 
 class TaskInfoBottom extends StatelessWidget {
+// void _timerContainer(BuildContext context) {
 
+// }
   
   @override
   Widget build(BuildContext context) {
    return new Container(
       padding: EdgeInsets.symmetric(horizontal: 6, vertical: 10),
       child: Material(
-        elevation: 19.0,
-        borderRadius: BorderRadius.all(Radius.circular(50)),
+        // elevation: 19.0,
+        borderRadius: BorderRadius.all(Radius.circular(200)),
         color: Colors.white,
         shadowColor: Colors.black,
         child: Container(
           width: 200,
           height: 200,
-          color: Colors.white,
-          child: 
-          Text('data',
+          // color: Colors.white,
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 60,
+              vertical: 80),
+          child:
+          Text('19:00',
           style: TextStyle(
             fontSize: 30
           ),),
-          
+          ),
         ),
       ),
     );
