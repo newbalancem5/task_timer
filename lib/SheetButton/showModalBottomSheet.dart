10ed9fdd33f7fe878411292b1527583ddab52ee7
newbalancem5 <newbalancem5@gmail.com>
2019-05-28 23:00:21 +0300

    Update showModalBottomSheet.dart
    Update desighn modal, add new bottom button, and new style

--- lib/SheetButton/showModalBottomSheet.dart
+++ lib/SheetButton/showModalBottomSheet.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:newba/Timer/TimerCount.dart';
 import 'package:flutter/cupertino.dart';
 
 
-TimerCountModel temerCountModel = TimerCountModel();
 class SheetModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -21,7 +19,7 @@ class SheetModal extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Container(
-                     padding: EdgeInsets.only(left: 16, right: 16, bottom: 30),
+                     padding: EdgeInsets.only(left: 16, right: 16, bottom: 80),
                      child: Title(
                        child: Text('Выберите время для задачи',
                     style: TextStyle(
@@ -36,32 +34,38 @@ class SheetModal extends StatelessWidget {
                     padding: EdgeInsets.only(bottom: 30),
                     height: MediaQuery.of(context).copyWith().size.height / 10,
                     child: CupertinoTimerPicker(
+                      
                       mode: CupertinoTimerPickerMode.hms,
                       minuteInterval: 1,
                       secondInterval: 1,
-                    onTimerDurationChanged: (Duration chanedtime){
-                        
+                    onTimerDurationChanged: (Duration chanedtime){                        
                     } ,
                 ) 
                 ),
-                ),                  
-                  Material(
-                    
-                    borderRadius: BorderRadius.circular(40.0),
-                    shadowColor: Colors.red,
-                    elevation: 6.0,
-                    color: Colors.red,
-                  
-                    child: MaterialButton(                    
-                      child: Text('добавить время',
-                      style:TextStyle(
-                        color: Colors.white
-                      ) ,),
-                      onPressed: (){
-                        // model.increment();
-                      },
-                    ),
-                  )
+                ),
+                   Container(                
+                padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+                child: Material(
+                  borderRadius: BorderRadius.circular(20.0),
+                  shadowColor: Colors.red,
+                  elevation: 6.0,
+                  color: Colors.red,
+                  child: MaterialButton(                    
+                    minWidth: 400.0,
+                    height: 60.0,
+                            onPressed: (){
+                              
+                  },
+                  child: Text('Добавить',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  ),
+                  ),
+                  ),
+                ),
+              ), 
                 ],
             ),
           );
