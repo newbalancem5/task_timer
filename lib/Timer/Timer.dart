eae3c386eb1d4d482837d63d341c3489c211d457
newbalancem5 <newbalancem5@gmail.com>
2019-06-11 16:00:58 +0300

    Update Timer.dart

--- lib/Timer/Timer.dart
+++ lib/Timer/Timer.dart
@@ -1,28 +1,6 @@
-// import 'package:flutter/material.dart';
-// import 'dart:math' as math;
-
-
-//   class TaskTimer extends StatefulWidget {
-//     @override
-//     _TaskTimerState createState() => _TaskTimerState();
-//   }
-  
-//   class _TaskTimerState extends State<TaskTimer> {
-
-//     _currentTime(){
-//     return "${DateTime.now().hour} : ${DateTime.now().minute}";
-//   }
-
-
-//     @override
-//     Widget build(BuildContext context) {
-//       return Scaffold(
-//         appBar: AppBar(
-//           title: Center(child: Text("TimeTasker"),),
-//           backgroundColor: Colors.deepOrange,
-//           elevation: 0.0,
-//         ),
-        
-//       );
-//     }
-//   }
+class Ticker {
+  Stream<int> tick({int ticks}) {
+    return Stream.periodic(Duration(seconds: 1), (x) => ticks - x - 1)
+        .take(ticks);
+  }
+}
